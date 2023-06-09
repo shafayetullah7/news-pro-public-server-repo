8b0cc8f06399d43113957447ec140389c41e5ec1
shafayetullah7 <shafayet769@gmail.com>
2023-06-09 11:55:50 +0600

    jwt verify and get user

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next)=>{
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access:no token'});
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access:invalid token'});
+    }
+    console.log('decoded : ',decoded);
+    req.decoded = decoded;
+    console.log('req.decoded: ',req.decoded);
+    next();
+  })
+}
+
 app.get('/',(req,res)=>{
     res.send('hello from news pro');
 })
@@ -27,6 +46,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
 
 async function run() {
   try {
@@ -51,6 +72,18 @@ async function run() {
     //     res.send(result);
     //     // res.send({});
     // })
+    app.get('/users/:email',verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+      const decodedEmail = req.decoded.data.email;
+      console.log('email: ',email);
+      console.log('dec email: ',decodedEmail);
+      
+      if(email!==decodedEmail){
+        return res.status(401).send({error:true,message:'unauthorized access:emails do not match'});
+      }
+      const result = await userCollection.findOne({email:email})
+      res.send(result);
+    })
 
     app.post('/users', async (req, res) => {
         const user = req.body;
